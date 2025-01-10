30a787468db3a1716e8daabab1c997eb29c9e8c9
Vahid Usetiful Github <vahid.alamipour@bilue.com.au>
2025-01-10 18:59:30 +1100

    fix(): update token

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import { Usetiful } from 'usetiful-react-native';
 export default function App() {
   return (
     <NavigationContainer>
-      <Usetiful token="65318218e671bc163c9306ddd37ccb3b">
+      <Usetiful token="34ae1d22e7615d614bd3a17920a907c0">
         <RootStack />
       </Usetiful>
     </NavigationContainer>
