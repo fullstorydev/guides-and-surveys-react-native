94f9e8aebf13b05e6a920f620e05f5deec6be71f
ammar <ammar@dobbytec.com>
2025-02-14 13:49:53 +0330

    feat(US-2576): applyig CR feedbacks

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,7 +1,6 @@
 import { ProgressBarHorizontal } from '../ProgressBar/Horizontal';
 import { ProgressBarDots } from '../ProgressBar/Dots';
 import { ProgressBarStepNumbers } from '../ProgressBar/StepNumbers';
-import { ProgressBarCircle } from '../ProgressBar/Circle';
 
 type ProgressProps = {
   progress: { state: boolean; type: number };
@@ -10,15 +9,12 @@ type ProgressProps = {
 export const RenderProgressBar = ({ progress }: ProgressProps) => {
   if (!progress.state) return null;
   switch (progress.type) {
-    case 1:
-      return <ProgressBarHorizontal />;
     case 2:
       return <ProgressBarDots />;
     case 3:
       return <ProgressBarStepNumbers />;
-    case 4:
-      return <ProgressBarCircle />;
+    case 1:
     default:
-      return null;
+      return <ProgressBarHorizontal />;
   }
 };
