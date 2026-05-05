bf31f6310c2c174e0d69bcb699b393598bc2ef3b
Ryan Wang <qwang1993@gmail.com>
2026-05-05 13:18:06 -0400

    get this to build

--- example/babel.config.js
+++ example/babel.config.js
@@ -10,7 +10,7 @@ module.exports = function (api) {
 
   return getConfig(
     {
-      presets: ['babel-preset-expo'],
+      presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
       plugins: isWeb
         ? []
         : ['@fullstory/react-native', 'react-native-worklets/plugin'],
